730c69fbb4717269ab059f158dcffee3c36fd144
Guilherme Miguel <guilherme.souza1996@hotmail.com>
2021-02-27 00:54:30 -0300

    Comeco do projeto uploadArquivo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Uploads_de_arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diretórioRemetente = Directory.GetDirectories(Console.ReadLine()).ToString();           
+
+            Console.WriteLine(diretórioRemetente);
+        }
+    }
+}
